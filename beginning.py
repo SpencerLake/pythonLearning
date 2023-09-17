ff9db16ae708c400481a5fb162db972581026a4b
Spencer-Lake <spencer.m.lake11@gmail.com>
2023-09-16 18:13:34 -0600

    first basics file. More of a file to terst my knowledge about basics

--- beginning.py
+++ beginning.py
@@ -48,3 +48,44 @@ for i in range(len(addList)):
 #Sort list
 fList.sort(reverse=True)
 print(fList)
+
+
+#DICTIONARIES
+
+fDictionary = {'key1':'hello'}
+sDictionary = {'key1':'hello', 'key2':'world'}
+tDictionary = {'key1':[{'key2':'hello'},'list sting']}
+gDictionary = {'key1':[1,2,{'key2':['nested string',{'key3':[1,2,['hello']]}]}]}
+
+fIndexing = fDictionary['key1']
+print(fIndexing)
+sIndexing = sDictionary['key2']
+print(sIndexing)
+tIndexing = tDictionary['key1'][0]['key2']
+print(tIndexing)
+gIndexing = gDictionary['key1'][2]['key2'][1]['key3'][2]
+print(gIndexing)
+
+
+#TUPLES
+
+#tuples are prettty much lists that are immutable (can't change). Everything else about them act the same as lists
+
+
+#SETS
+
+#Pretty much unordered lists that are distinct. Imagine running a SELECT DISTINCT query in SQL
+
+
+
+#OPERATORS (BOOLEANS)
+
+#  == is equal to
+#  != is NOT equal to
+#  > greater than
+#  < less than
+#  >= greater than or equal to
+#  <= less than or equal to
+
+
+
